94a32934a28a0eb1be7c234ad45ba13ec6c0b717
zdebikk <zdebkacper6@gmail.com>
2026-09-18 20:17:19 +0200

    varepsilon - zmieniable

--- moje_rybki+.py
+++ moje_rybki+.py
@@ -41,7 +41,7 @@ def main():
     def update_caption():
         status = "Running" if not paused else "Paused"
         pygame.display.set_caption(
-            f"Lotka-Volterra [{status}] at {current_fps} FPS | Grow: {r_grow:.3f} | Eat: {p_eat:.3f} | Die: {d_die:.3f} | 'P' to Plot"
+            f"My Fish [{status}] at {current_fps} FPS | Grow: {r_grow:.3f} | Eat: {p_eat:.3f} | Die: {d_die:.3f} | Epsilon: {varepsilon:.3f} | 'P' to Plot"
         )
 
     update_caption()
@@ -90,6 +90,10 @@ def main():
                     d_die = min(1.0, d_die + 0.01); update_caption()
                 elif event.key == pygame.K_d:
                     d_die = max(0.0, d_die - 0.01); update_caption()
+                elif event.key == pygame.K_t:
+                    varepsilon = min(1.0, varepsilon + 0.001); update_caption()
+                elif event.key == pygame.K_g:
+                    varepsilon = max(0.0, varepsilon - 0.01); update_caption()
 
 
                 # --- Speed Control Logic ---
